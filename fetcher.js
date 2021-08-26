155a17870683877edc4447b7d3db24a9f209bf87
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-26 01:13:14 +0000

    added console.log to say how many bytes were downloaded

--- fetcher.js
+++ fetcher.js
@@ -1,20 +1,16 @@
 const request = require('request');
 const fs = require('fs');
 const args = process.argv.slice(2);
-console.log('args:', args);
 const httpAddress = args[0];
 const file = args[1]
-console.log('args[1]:', args[1]);
-console.log('file', file);
 //console.log('args', args);
 
 const webInfo = request(httpAddress, (error, response, body) => {
   console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
   const bodyInfo = ('body:', body);
   fs.writeFile(`./${file}`, bodyInfo, err => {
-    if (err) {
-      console.log(err);
-    }
   });
 });
+
+const stats = fs.statSync('./index.html');
+console.log(`Downloaded and saved ${stats.size} bytes to ${file}`);
